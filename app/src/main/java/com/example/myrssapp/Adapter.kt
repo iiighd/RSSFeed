58970abf449f9fcd3c392b7d6eae5b439a8a6208
Win10 <iiighd19@gmail.com>
2021-12-21 22:01:48 +0300

    rss

---
+++ app/src/main/java/com/example/myrssapp/Adapter.kt
@@ -0,0 +1,36 @@
+package com.example.myrssapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myrssapp.databinding.QuestionItemBinding
+
+class Adapter (private val questions: ArrayList<Question>, private val context: Context):
+    RecyclerView.Adapter<Adapter.ItemViewHolder>() {
+    class ItemViewHolder(val binding: QuestionItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(QuestionItemBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val question = questions[position]
+        holder.binding.apply {
+            titleTv.text = question.title
+            authorTv.text = "Asked by: ${question.author}"
+            categoriesTv.text = question.categories
+            questionCard.setOnClickListener {
+                Alert(context, question.title, question.details)
+            }
+        }
+    }
+
+    override fun getItemCount() = questions.size
+}
+
+enum class QuestionItemBinding {
+
+}
---
+++ app/src/main/java/com/example/myrssapp/Questions.kt
@@ -0,0 +1,3 @@
+package com.example.myrssapp
+
+data class Question (val title: String, val author: String, var categories: String, val details: String)
